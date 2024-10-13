14b9aeb31355bf6da0831c41d7d7791be9ccd568
fppinedajr <fernandojr03.pineda@gmail.com>
2024-10-14 00:47:04 +0800

    Optimize med num2

--- MySols/Medium/2.bat
+++ MySols/Medium/2.bat
@@ -6,5 +6,12 @@ set /p num2="Enter second number: "
 set /p num3="Enter third number: "
 
 echo.
-if %num1% LSS %num2% if %num2% LSS %num3% echo Numbers are in ascending order: %num1%, %num2%, %num3%
-if %num1% GTR %num2% if %num2% GTR %num3% echo Numbers are in descending order: %num3%, %num2%, %num1%
+if %num1% LSS %num2% if %num2% LSS %num3% (
+    echo Numbers are in ascending order: %num1%, %num2%, %num3%
+    exit
+)
+if %num1% GTR %num2% if %num2% GTR %num3% (
+    echo Numbers are in descending order: %num1%, %num2%, %num3%
+    exit
+)
+echo Numbers are not in order
