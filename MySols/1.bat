937d3f5905b7940bcffe236f8ebef7512a6b5f8e
fppinedajr <fernandojr03.pineda@gmail.com>
2024-10-13 23:54:17 +0800

    Optimize code for num1

--- MySols/1.bat
+++ MySols/1.bat
@@ -3,10 +3,10 @@ cls
 
 set /p num1="Enter the first number: "
 set /p num2="Enter the second number: "
-set /a sum = %num1% + %num2%
-set /a difference = %num1% - %num2%
-set /a product = %num1% * %num2%
-set /a quotient = %num1% / %num2%
+set /a sum = num1 + num2
+set /a difference = num1 - num2
+set /a product = num1 * num2
+set /a quotient = num1 / num2
 
 echo.
 echo Sum: %sum%
